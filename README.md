8fdd7113734730416f139c21b788646ff9f7e3b4
krymancer <junior.nascm@gmail.com>
2020-09-02 00:25:03 -0300

    Push

---
+++ README.md
@@ -0,0 +1,6 @@
+# Twitter React UI Clone
+A clone of Twitter UI using ReacJS and Typescript [Deployed here]()
+
+<p align="center">
+  <img alt="Dropbox" src=".github/twitter.png">
+</p>
---
+++ src/components/ProfilePage/index.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+
+import Feed from '../Feed';
+
+import {
+  Container,
+  Banner,
+  Avatar,
+  ProfileData,
+  LocationIcon,
+  CakeIcon,
+  Followage,
+  EditButton,
+} from './styles';
+
+const ProfilePage: React.FC = () => {
+  return (
+    <Container>
+      <Banner>
+        <Avatar />
+      </Banner>
+
+      <ProfileData>
+        <EditButton outlined>Editar perfil</EditButton>
+
+        <h1>Júnior Nascimento</h1>
+        <h2>@juniornascm</h2>
+
+        <p>
+        Computer Engineer @ UFC
+        </p>
+
+        <ul>
+          <li>
+            <LocationIcon />
+            Ceará, Brasil
+          </li>
+          <li>
+            <CakeIcon />
+            Nascido(a) em 01 de junho de 2000
+          </li>
+        </ul>
+
+        <Followage>
+          <span>
+            seguindo <strong>94</strong>
+          </span>
+          <span>
+            <strong>672 </strong> seguidores
+          </span>
+        </Followage>
+      </ProfileData>
+
+      <Feed />
+    </Container>
+  );
+};
+
+export default ProfilePage;
