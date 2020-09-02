9cfc52f637a028d023cab1530cbb978a791757e1
krymancer <junior.nascm@gmail.com>
2020-09-02 00:28:09 -0300

    Updates

---
+++ precache-manifest.a995891694050f45c370e4d8662e4837.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ddf132e70ed3f4fef5782c7f37c6f5f",
+    "url": "/twitter-ui-clone/index.html"
+  },
+  {
+    "revision": "13b85a721c5fb82aa1dc",
+    "url": "/twitter-ui-clone/static/js/2.ce66068e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/twitter-ui-clone/static/js/2.ce66068e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "176b777a15fa6cee4591",
+    "url": "/twitter-ui-clone/static/js/main.60594cea.chunk.js"
+  },
+  {
+    "revision": "9cdbd9cb544dee6c9ccc",
+    "url": "/twitter-ui-clone/static/js/runtime-main.8cf8e266.js"
+  }
+]);
